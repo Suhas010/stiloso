b56591ec025bf971c7a174137dce49cd8ce323af
Andrea Parodi <andrea@parro.it>
2017-05-05 20:58:48 +0200

    API

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ const isStyle = def => def !== null && typeof def === 'object';
 const isDynamicFn = def => typeof def === 'function';
 const applyProps = props => fn => fn(props);
 
+/**
+ * Create a preact component that render to the specified HTML element, applying
+ * specified classes and styles.
+ * Component children will become children of the HTML element, and props will be
+ * forwarded to it as well. Class names and inline CSS styles will be coinstructed as specified below.
+ *
+ * @param  {String} tagName Tagname of the html element to use for the component
+ * @param  {Object|Function|String}definers Arguments that defines classes and styles to apply to the html element.
+ *   Array elements could have following types:
+ *     - String:  argument value will be used as a class of the HTML element
+ *     - Object:  argument will be used as style property of the element, using normal react syntax
+ *     - Function:  provided function will be called with component props as argument. The function should return a string or an array of string that will be used as classes of the HTML element.
+ * @return {Component} A stateless preact component.
+ */
 export const stiloso = (tagName, ...definers) => {
 	const staticClassNames = definers.filter(isClassName);
 	const staticStyles = definers.filter(isStyle);
